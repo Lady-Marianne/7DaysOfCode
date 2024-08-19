8cf65ca752fd90a3586df5d0a6c85dfcf79f27fe
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-08-19 13:53:11 -0300

    Día 3 y 4

--- app.js
+++ app.js
@@ -34,12 +34,64 @@ let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
 
 alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}.`);
 
+let gustoPorLenguaje = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO:`);
 
-// DÍA 3:
+if (gustoPorLenguaje == '1') {
+    alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+} else if (gustoPorLenguaje == '2') {
+    alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
+} else {
+    alert("Respuesta no válida. Por favor, ingresa 1 para SÍ o 2 para NO.");
+}
 
-// DÍA 4:
+// Lógica JS 3/7: Estructuras de control de flujo:
 
-// DÍA 5:
+const area = prompt("¿Quieres seguir en el área de 'Front-End' o 'Back-End'? Escribe el nombre del área:");
+lenguaje = "";
+if (area === "Front-End") {
+    lenguaje = prompt("¿Quieres aprender React o Vue?");
+} else if (area === "Back-End") {
+    lenguaje = prompt("¿Quieres aprender C# o Java?");
+} else {
+    alert("¡No ingresaste un área válida!");
+}
+
+const especialidadOFullstack = prompt("Escribe 1 para seguir especializándote en el área elegida o 2 para desarrollarte para convertirte en Fullstack");
+if (especialidadOFullstack == 1) {
+    alert("¡Sigue especializándote en ${lenguaje} para dominar el área de ${area}!");
+} else if (especialidadOFullstack == 2) {
+    alert("¡Es hora de comenzar a aprender otros lenguajes además de ${lenguaje} si quieres convertirte en Fullstack!");
+} else {
+    alert("¡No ingresaste un valor válido!");
+}
+
+let mensaje = prompt("¿Hay alguna otra tecnología que te gustaría aprender? Escribe 'ok' en caso positivo.");
+while (mensaje === "ok") {
+    let nuevaTecnología = prompt("¿Cuál?");
+    alert(`¡${nuevaTecnología} es realmente una tecnología muy interesante!`);
+    mensaje = prompt("¿Hay alguna otra tecnología que te gustaría aprender? Escribe 'ok' en caso positivo.");
+}
+
+// Lógica JS 4/7: Mas loops y randomización:
+
+const numeroAdivinacion = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let intento = "";
+let acerto = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    intento = prompt("Intenta adivinar el número del 0 al 10:");
+    if(intento == numeroAdivinacion){
+        alert("¡Felicidades, acertaste! El número era ${numeroAdivinacion}.");
+        acerto = true;
+        break;
+    }
+    alert("¡Incorrecto!");
+}
+if(!acerto){
+    alert("Desafortunadamente, no acertaste. ¡El número era ${numeroAdivinacion}!");
+}
+
+// Lógica JS 5/7: Arrays y colecciones:
 
 // DÍA 6:
 

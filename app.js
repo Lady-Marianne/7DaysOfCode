ffe2576f64d94dcfbd53d481173c410fd3f5135b
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-08-23 07:12:31 -0300

    Debo revisar todo

--- app.js
+++ app.js
@@ -128,6 +128,121 @@ while(agregarMas != "no"){
 }
 alert("Lista de compras:\n  Frutas: ${frutas}\n  Lácteos: ${lacteos}\n  Dulces: ${dulces}\n  Congelados: ${congelados}");
 
-// DÍA 6:
+// Lógica JS 6/7: Remoción de arrays:
 
-//  DÍA 7:
+let frutas = [];
+let lacteos = [];
+let dulces = [];
+let congelados = [];
+let comida = "";
+let categoria = "";
+let eliminar = "";
+
+let agregarMas = "sí";  // valor inicial como "sí", porque la primera vez siempre va a entrar en el while
+while(agregarMas != "no"){
+    if(frutas.length === 0 && lacteos.length === 0 && dulces.length === 0 && congelados.length === 0){
+        agregarMas = prompt("¿Deseas agregar una comida a la lista de compras? Responde 'sí' o 'no'.");
+    } else {
+        agregarMas = prompt("¿Deseas agregar una comida a la lista de compras? Responde 'sí', 'no' o 'eliminar'.");
+    }
+	
+    while (agregarMas != "sí" && agregarMas != "no" && agregarMas != "eliminar") {  //mientras el texto leído sea diferente de "sí", "no" y "eliminar", mostrar que no fue reconocido y preguntar nuevamente
+	alert(`¡Operación no reconocida!`);
+        agregarMas = prompt("¿Deseas agregar una comida a la lista de compras? Responde 'sí' o 'no'.");
+    }
+	
+    if (agregarMas === "no"){  //si el texto leído es "no", salir del while
+	break;
+    }
+	
+    if(agregarMas === "sí"){
+        comida = prompt("¿Qué comida deseas agregar?");
+        categoria = prompt("¿En qué categoría encaja esta comida: 'frutas', 'lacteos', 'dulces' o 'congelados'?");
+        if(categoria === 'frutas'){
+            frutas.push(comida);
+        } else if (categoria === 'lacteos'){
+            lacteos.push(comida);
+        } else if (categoria === 'dulces'){
+            dulces.push(comida);
+        } else if (categoria === 'congelados'){
+            congelados.push(comida);
+        } else {
+            alert("Esa categoria no está predefinida.")
+        }
+    } else if(agregarMas === "eliminar"){
+	if(frutas.length === 0 && lacteos.length === 0 && dulces.length === 0 && congelados.length === 0){  //si la lista está vacía (tratamiento de bug, en caso de que la persona escriba "eliminar" incluso cuando solo se muestren las opciones "sí" y "no")
+		alert(`¡La lista está vacía!`);
+	} else {  //si la lista no está vacía
+		eliminar = prompt(`Lista de compras:\n  Frutas: ${frutas}\n  Lácteos: ${lacteos}\n  Dulces: ${dulces}\n  Congelados: ${congelados}\n\n¿Qué producto deseas eliminar?`);
+		if(frutas.indexOf(eliminar) != -1){
+			frutas.splice(frutas.indexOf(eliminar), 1);
+			alert(`¡El ítem ${eliminar} ha sido eliminado con éxito!`)
+		} else if(lacteos.indexOf(eliminar) != -1){
+			lacteos.splice(lacteos.indexOf(eliminar), 1);
+			alert(`¡El ítem ${eliminar} ha sido eliminado con éxito!`)
+		} else if (dulces.indexOf(eliminar) != -1){
+			dulces.splice(dulces.indexOf(eliminar), 1);
+			alert(`¡El ítem ${eliminar} ha sido eliminado con éxito!`)
+		} else if (congelados.indexOf(eliminar) != -1){
+			congelados.splice(congelados.indexOf(eliminar), 1);
+			alert(`¡El ítem ${eliminar} ha sido eliminado con éxito!`)
+		} else {
+			alert(`¡No fue posible encontrar el ítem dentro de la lista!`)
+		}
+	}
+    }
+}
+alert(`Lista de compras:\n  Frutas: ${frutas}\n  Lácteos: ${lacteos}\n  Dulces: ${dulces}\n  Congelados: ${congelados}`);
+
+//  Lógica JS 7/7: Funciones en javascript:
+
+function suma(valor1, valor2) {
+    return Number(valor1) + Number(valor2);
+}
+
+function resta(valor1, valor2) {
+    return Number(valor1) - Number(valor2);
+}
+
+function multiplicacion(valor1, valor2) {
+    return Number(valor1) * Number(valor2);
+}
+
+function division(valor1, valor2) {
+    return Number(valor1) / Number(valor2);
+}
+
+let valor1;
+let valor2;
+let operacion = "";
+
+do {  //usando 'do...while', ya que la primera vez siempre vamos a entrar
+    
+    operacion = prompt(`¿Qué operación quieres realizar? Responde 'suma', 'resta', 'multiplicación', 'división' o 'salir'.`);
+    while (operacion != "suma" && operacion != "resta" && operacion != "multiplicación" && operacion != "división" && operacion != "salir") {  //mientras el texto leído sea diferente de "suma", "resta", "multiplicación", "división" y "salir", mostrar que no fue reconocido y preguntar nuevamente
+        alert(`¡Operación no reconocida!`);
+        operacion = prompt(`¿Qué operación quieres realizar? Responde 'suma', 'resta', 'multiplicación', 'división' o 'salir'.`);
+    }
+    
+    if (operacion === "salir") {  //si el texto leído es "salir", salir del loop y de la calculadora
+        break;
+    }
+    
+    valor1 = prompt(`Ingresa el primer valor:`);
+    valor2 = prompt(`Ingresa el segundo valor:`);
+    switch (operacion) {
+        case 'suma':
+            alert(`El resultado de la ${operacion} es ${suma(valor1, valor2)}`);
+            break;
+        case 'resta':
+            alert(`El resultado de la ${operacion} es ${resta(valor1, valor2)}`);
+            break;
+        case 'multiplicación':
+            alert(`El resultado de la ${operacion} es ${multiplicacion(valor1, valor2)}`);
+            break;
+        case 'división':
+            alert(`El resultado de la ${operacion} es ${division(valor1, valor2)}`);
+            break;
+    }
+} while (operacion === "suma" || operacion === "resta" || operacion === "multiplicación" || operacion === "división")
+alert(`¡Hasta la próxima!`);
